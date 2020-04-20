f3f132a73758f7100259f16713a3399fdea3df52
regnore <34236060+regnore@users.noreply.github.com>
2020-04-21 00:16:34 +0800

    Add files via upload

---
+++ ConcurrentSocket/src/Client/Client.cpp
@@ -0,0 +1,70 @@
+﻿// Client.cpp
+
+#include "pch.h"
+#include<WinSock2.h>
+#include<WS2tcpip.h>
+#include <iostream>
+
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+	WSADATA wsd;
+	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0) {
+		WSACleanup();
+		return -1;
+	}
+
+	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET) {
+		cout << "ERROR:" << WSAGetLastError() << endl;
+		WSACleanup();
+		return -2;
+	}
+
+	SOCKADDR_IN client;
+	client.sin_family = AF_INET;
+	client.sin_port = htons(6863);
+	inet_pton(AF_INET, "127.0.0.1", (void*)&client.sin_addr.S_un.S_addr);
+
+	int const SERVER_MSG_SIZE = 128;
+	char ID[SERVER_MSG_SIZE] = { 0 };
+	char inMSG[SERVER_MSG_SIZE] = { 0 };
+	char outMSG[SERVER_MSG_SIZE];
+
+	if (connect(clientSocket, (struct sockaddr*) &client, sizeof(client)) < 0) {
+		cout << "ERROR:" << WSAGetLastError() << endl;
+		closesocket(clientSocket);
+		WSACleanup();
+		return -3;
+	}
+
+	else {
+		cout << "Connect to Server Successfully." << endl;
+		int size = recv(clientSocket, inMSG, SERVER_MSG_SIZE, 0);
+		strcpy_s(ID, inMSG);
+		cout << "Your Thread ID :" << inMSG << endl<<endl;
+		memset(inMSG, 0, SERVER_MSG_SIZE);
+		while (true) {
+			memset(outMSG, 0, SERVER_MSG_SIZE);
+			cout << "Please Input the Command (Input \"$help\" to show the command direction.):" << endl;
+			cin >> outMSG;
+			send(clientSocket, outMSG, SERVER_MSG_SIZE, 0);
+			if (strcmp(outMSG, "$quit") == 0) {
+				break;
+			}
+			int size = recv(clientSocket, inMSG, SERVER_MSG_SIZE, 0);
+			cout << "Server Response：\n" << inMSG << endl;
+			memset(inMSG, 0, SERVER_MSG_SIZE);
+		}
+	}
+
+	closesocket(clientSocket);
+	WSACleanup();
+
+	system("pause");
+	return 0;
+
+}
---
+++ ConcurrentSocket/src/Server/Server.cpp
@@ -0,0 +1,141 @@
+﻿// Server.cpp
+
+#include "pch.h"
+#include<WinSock2.h>
+#include<WS2tcpip.h>
+#include <iostream>
+
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib")
+
+DWORD threadID = 0;
+int currentClientNum = 0;
+
+DWORD WINAPI clientChildThread(LPVOID ipParameter) {
+	SOCKET clientSocket = (SOCKET)ipParameter;
+	int ID = ++threadID;
+	char IDString[10];
+	int const CLIENT_MSG_SIZE = 128;
+	char inMSG[CLIENT_MSG_SIZE];
+	char outMSG[CLIENT_MSG_SIZE];
+	char wx[] = "Invalid Command.";
+	int size;
+	memset(inMSG, 0, CLIENT_MSG_SIZE);
+	sprintf_s(outMSG, "%03d",ID);
+	send(clientSocket, outMSG, CLIENT_MSG_SIZE, 0);
+	sprintf_s(IDString, "%03d", ID);
+	currentClientNum++;
+	cout << "Thread "<<IDString<<" starts to serve." <<"("<<currentClientNum<<")"<< endl;
+	while (true) {
+		memset(inMSG, 0, CLIENT_MSG_SIZE);
+		size = recv(clientSocket, inMSG, CLIENT_MSG_SIZE, 0);
+		if (size == SOCKET_ERROR) {
+			cout << "Connection Interruptted，ERROR：" << WSAGetLastError() << endl;
+			closesocket(clientSocket);
+			break;
+		}
+		cout << "Client "<<IDString<<" :" << inMSG << endl;
+		if (strcmp(inMSG, "$help") == 0) {
+			sprintf_s(outMSG, " $help\t\tShow Command Direction.\n $time\t\tShow Server Current Time.\n $quit\t\tTerminate the Connection.");
+			send(clientSocket, outMSG, CLIENT_MSG_SIZE, 0);
+		}
+		else if (strcmp(inMSG, "$time") == 0) {
+			SYSTEMTIME systime = { 0 };
+			GetLocalTime(&systime);
+			sprintf_s(outMSG, "%d-%02d-%02d %02d:%02d:%02d",
+				systime.wYear, systime.wMonth, systime.wDay,
+				systime.wHour, systime.wMinute, systime.wSecond);
+			send(clientSocket, outMSG, CLIENT_MSG_SIZE, 0);
+		}
+		else if (strcmp(inMSG, "$quit") == 0) {
+			closesocket(clientSocket);
+			currentClientNum--;
+			cout << IDString << " Connection Terminated Successfully.(" << currentClientNum << ")" << endl;
+			if (currentClientNum == 0)
+				cout << "\nWaiting for the Client Connection.(0)" << endl;
+			break;
+		}
+		else {
+			send(clientSocket, wx, sizeof(wx), 0);
+		}
+	}
+
+	return 0;
+
+}
+
+
+int main()
+{
+	WSADATA wsd;
+	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0) {
+		WSACleanup();
+		return -1;
+	}
+
+	SOCKET serverSocket;
+	serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (serverSocket == INVALID_SOCKET) {
+		cout << "ERROR:" << WSAGetLastError() << endl;
+		WSACleanup();
+		return -2;
+	}
+
+	SOCKADDR_IN server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(6863);
+	server.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+
+	if (bind(serverSocket, (struct sockaddr*) &server, sizeof(server)) == SOCKET_ERROR) {
+		cout << "ERROR:" << WSAGetLastError() << endl;
+		closesocket(serverSocket);
+		WSACleanup();
+		return -3;
+	}
+
+	if (listen(serverSocket, 2) == SOCKET_ERROR) {
+		cout << "ERROR:" << WSAGetLastError() << endl;
+		closesocket(serverSocket);
+		WSACleanup();
+		return -4;
+	}
+
+	cout << "\nWaiting for the Client Connection.(0)" << endl;
+
+	SOCKET clientSocket;
+	SOCKADDR_IN client;
+	int addrsize = sizeof(SOCKADDR_IN);
+	HANDLE pThread;
+	while (true) {
+
+		clientSocket = accept(serverSocket, (struct sockaddr*) &client, &addrsize);
+		if (clientSocket == INVALID_SOCKET) {
+			cout << "Client Accept Failed，ERROR：" << WSAGetLastError() << endl;
+			closesocket(serverSocket);
+			WSACleanup();
+			return -5;
+		}
+		else {
+			char sendBuf[20] = { '\0' };
+			cout << "Client\n"
+				<< inet_ntop(AF_INET, (void*) &client.sin_addr, sendBuf, 16)
+				<< "\nVia Port：\n"
+				<< ntohs(client.sin_port)
+				<< "\nConnection Succeed." << endl;
+			pThread = CreateThread(NULL, 0, clientChildThread, (LPVOID)clientSocket, 0, NULL);
+			if (pThread == NULL) {
+				cout << "Failed to Create the Thread." << endl;
+				break;
+			}
+			CloseHandle(pThread);
+
+		}
+	}
+
+	closesocket(serverSocket);
+	closesocket(clientSocket);
+	WSACleanup();
+
+	return 0;
+}
